7620b19c5557aa4fea4f0c98defbb2ae05a1c6d9
Bipin-codder <151555645+Bipin-codder@users.noreply.github.com>
2023-11-22 12:23:58 +0530

    Add files via upload

---
+++ Persistence/ConnectionDBContext.cs
@@ -0,0 +1,40 @@
+﻿using Domain;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class ConnectionDBContext : DbContext
+    {
+        public ConnectionDBContext(DbContextOptions<ConnectionDBContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ConnectionDBContext).Assembly);            
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModfiedDate = DateTime.Now;
+
+                if (entry.State == null)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public DbSet<Attributes> Attributes { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Application.Persistence.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ConnectionDBContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+            service.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            service.AddScoped<IProductRepository, ProductRepository>();
+            //service.AddScoped<IAttributeRepository, >();
+            //service.AddScoped<ICategoryRepository, >();
+
+            return service;
+            
+        }
+    }
+}
