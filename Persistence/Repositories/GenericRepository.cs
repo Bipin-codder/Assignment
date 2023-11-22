4e1e3a45253413728f407211108251579a7aa638
Bipin-codder <151555645+Bipin-codder@users.noreply.github.com>
2023-11-22 12:25:01 +0530

    Add files via upload

---
+++ Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Application.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T :class
+    {
+        private readonly ConnectionDBContext _dbContext;
+        public GenericRepository(ConnectionDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public async Task<T> Add(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;            
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();            
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);          
+        }
+
+        public Task<IReadOnlyList<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<T> IGenericRepository<T>.Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Application.Persistence.Contracts;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly ConnectionDBContext _dbContext;
+        public ProductRepository(ConnectionDBContext dBContext) : base(dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public async Task<Product> GetProductWithDetails(int id)
+        {
+            var Product = await _dbContext.Product.Include(q => q.Category).Include(x => x.ProductAttributes)
+                            .FirstOrDefaultAsync(y => y.Id == id);
+
+            return Product;
+        }
+
+        public async Task<List<Product>> GetProductWithDetails()
+        {
+            var Products = await _dbContext.Product.Include(q => q.Category)
+                                .Include(x => x.ProductAttributes).ToListAsync();
+            return Products;            
+        }
+    }
+}
